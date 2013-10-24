8aad899f8e83cfabcb39b4f4d36a302e6484c9b1
Brian Schultek <schultbr@gmail.com>
2013-10-23 21:54:42 -0400

    BRS: Command line stuff is seeming to work. Now we can dig into real modules. I'll start on this tomorrow most likely

--- src/utilities/commandLine/CommandLine.c
+++ src/utilities/commandLine/CommandLine.c
@@ -16,7 +16,7 @@ void print_usage() {
     printf("                 --fu NUM --rnt NUM --rob NUM \n");
     printf("                 --l1hr NUM --l1at NUM --l2hr NUM \n");
     printf("                 --l2at NUM --trace TRACE_FILE\n");
-    printf("All parameters ARE required.");
+    printf("All parameters ARE required. ");
     printf("See included test.sh for usage examples\n");
 }
 
@@ -53,34 +53,67 @@ int processCommandLine(int argc, char **argv,
 	int opt = 0;
 	int long_index = 0;
 
-	while ((opt = getopt_long_only(argc, argv,"",
+	printf("Processing command line for %d options\n", argc);
+
+	while ((opt = getopt_long_only(argc, argv,"s:b:r:f:n:1:a:2:c:t:",
 				   long_options, &long_index )) != -1) {
+//		printf("Found opt=%d\n", opt);
 		switch (opt) {
-			 case 's' : *superScalarFactor = atoi(optarg);
+			 case 's' :
+				 printf("Found s\n");
+				 *superScalarFactor = atoi(optarg);
 				 break;
-			 case 'b' : *btbSize = atoi(optarg);
+			 case 'b' :
+				 printf("Found b\n");
+				 *btbSize = atoi(optarg);
 				 break;
-			 case 'r' : *rsEntries = atoi(optarg);
+			 case 'r' :
+				 printf("Found r\n");
+				 *rsEntries = atoi(optarg);
 				 break;
-			 case 'f' : *fuCount = atoi(optarg);
+			 case 'f' :
+				 printf("Found f\n");
+				 *fuCount = atoi(optarg);
 				 break;
-			 case 'n' : *renameTableEntries = atoi(optarg);
+			 case 'n' :
+				 printf("Found n\n");
+				 *renameTableEntries = atoi(optarg);
 				 break;
-			 case 'o' : *reorderBufferEntries = atoi(optarg);
+			 case 'o' :
+				 printf("Found o\n");
+				 *reorderBufferEntries = atoi(optarg);
 				 break;
-			 case '1' : *level1CacheHitRate = atoi(optarg);
+			 case '1' :
+				 printf("Found 1\n");
+				 *level1CacheHitRate = atoi(optarg);
 				 break;
-			 case 'a' : *level1CacheAccessTime = atoi(optarg);
+			 case 'a' :
+				 printf("Found a\n");
+				 *level1CacheAccessTime = atoi(optarg);
 				 break;
-			 case '2' : *level2CacheHitRate = atoi(optarg);
+			 case '2' :
+				 printf("Found 2\n");
+				 *level2CacheHitRate = atoi(optarg);
 				 break;
-			 case 'c' : *level2CacheAccessTime = atoi(optarg);
+			 case 'c' :
+				 printf("Found c\n");
+				 *level2CacheAccessTime = atoi(optarg);
 				 break;
-			 case 't' : inputTrace = optarg;
+			 case 't' :
+				 printf("Found t\n");
+				 inputTrace = optarg;
 				 break;
-			 default: print_usage();
+			 default:
+				 printf("Found default\n");
+				 print_usage();
 				 exit(EXIT_FAILURE);
 		}
+	}
+
+	if(opt == -1) {
+		printf("Found opt=%d\n", opt);
+		print_usage();
+		exit(EXIT_FAILURE);
 	}
 
 	return 0; //return 0 for success, <0 for failure. handle that on the calling side.
---
+++ test.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+EXE=./bin/project2
+
+TRACE=./traces/compress.tra
+
+SS_FACTOR=1
+BTB_SIZE=1
+RS_STATION_SIZE=1
+FU_COUNT=1
+RENAME_TABLE_SIZE=1
+ROB_SIZE=10
+L1_HR=1
+L1_AT=15
+L2_HR=1
+L2_AT=15
+
+$(EXE) --ss $(SS_FACTOR) --btb $(BTB_SIZE) --rs $(RS_STATION_SIZE) -fu $(FU_COUNT) \
+--rnt $(RENAME_TABLE_SIZE) --rob $(ROB_SIZE) --l1hr $(L1_HR) --l1at $(L1_AT) \ 
+--l2hr $(L2_HR) --l2at $(L2_AT) --trace $(TRACE)
+
+
+
