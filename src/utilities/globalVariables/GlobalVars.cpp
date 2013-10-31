bdeb312f24d6e64236a9280cd273723e111c2b55
Brian Schultek <schultbr@gmail.com>
2013-10-30 22:40:55 -0400

    BRS: Changed how globals were being used a bit to make things better

--- src/utilities/globalVariables/GlobalVars.cpp
+++ src/utilities/globalVariables/GlobalVars.cpp
@@ -5,16 +5,36 @@
  *      Author: brian
  */
 #include "GlobalVars.h"
+#include <vector>
 
-int superScalarSize;
-TraceReader instructionTrace;
 
-//these setters are totally unnecessary but, meh. theyre already here.
-//kinda the right thing to do... sometimes
-void setSuperScalarSize(int size) {
-	superScalarSize = size;
-}
+int returnVal = -1;
+int superScalarFactor = -1;
+int btbSize = -1;
+int rsEntries = -1;
+int fuCount = -1;
+int renameTableEntries = -1;
+int reorderBufferEntries = -1;
+float instrCacheHitRate = -1;
+int instrCacheAccessTime = -1;
+float level1CacheHitRate = -1;
+int level1CacheAccessTime = -1;
+float level2CacheHitRate = -1;
+int level2CacheAccessTime = -1;
+char inputTrace[50];
 
-void setInstructionTrace(char *file) {
-	instructionTrace.openTrace(file);
-}
+std::vector<int> registers;
+std::vector<int> fpRegisters;
+int regHILO = 0;
+int regFCC = 0;
+
+
+////these setters are totally unnecessary but, meh. theyre already here.
+////kinda the right thing to do... sometimes
+//void setSuperScalarSize(int size) {
+//	superScalarSize = size;
+//}
+//
+//void setInstructionTrace(char *file) {
+//	instructionTrace.openTrace(file);
+//}
--- src/utilities/globalVariables/GlobalVars.h
+++ src/utilities/globalVariables/GlobalVars.h
@@ -8,12 +8,32 @@
 #ifndef GLOBALVARS_H_
 #define GLOBALVARS_H_
 
-#include "TraceReader.h"
+//#include "TraceReader.h"
+#include <vector>
 
-extern TraceReader instructionTrace;
-extern int superScalarSize;
+extern int superScalarFactor;
+extern int btbSize;
+extern int rsEntries;
+extern int fuCount;
+extern int renameTableEntries;
+extern int reorderBufferEntries;
+extern float instrCacheHitRate;
+extern int instrCacheAccessTime;
+extern float level1CacheHitRate;
+extern int level1CacheAccessTime;
+extern float level2CacheHitRate;
+extern int level2CacheAccessTime;
+extern char inputTrace[50];
 
-void setSuperScalarSize(int size);
-void setInstructionTrace(char *file);
+
+extern std::vector<int> registers;
+extern std::vector<int> fpRegisters;
+extern int regHILO;
+extern int regFCC;
+
+
+//
+//void setSuperScalarSize(int size);
+//void setInstructionTrace(char *file);
 
 #endif /* GLOBALVARS_H_ */
